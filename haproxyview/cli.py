1f60889fdc1f9485bb8cfa0ec65f16c42d62e63b
Bradley Cicenas <bradley.cicenas@gmail.com>
2015-08-27 15:05:52 -0400

    refactor poller and webapp to remove redis requirement. update cli to start both components.

--- haproxyview/cli.py
+++ haproxyview/cli.py
@@ -2,30 +2,34 @@ import os
 import sys
 import yaml
 from argparse import ArgumentParser
-from version import __version__
-from poller import Poller
+
+from version import version 
+from app import HaproxyView, Poller 
 
 #defaults
 config = { 'user': None,
            'pass': None,
-           'interval': 5 }
+           'interval': 10 }
 
-if __name__ == '__main__':
-    parser = ArgumentParser(description='haproxyview %s' % (__version__))
+def main():
+    parser = ArgumentParser(description='haproxyview %s' % (version))
     parser.add_argument('-c',
-                        dest='config_file',
-                        help='Path to config file (default: ./config.yml)',
+                        dest='config_path',
+                        help='Path to config file',
                         default='config.yml')
 
     args = parser.parse_args()
-    config_file = os.path.expanduser(args.config_file)
+    config_path = os.path.expanduser(args.config_path)
 
-    with open(config_file) as of:
+    with open(config_path) as of:
         config.update(yaml.load(of.read()))
 
-    p = Poller(
-            config['servers'],
-            user=config['user'],
-            password=config['pass'],
-            interval=config['interval']
-            )
+    print(config['servers'])
+    poller = Poller(config['servers'], config['user'], config['pass'])
+    poller.start(config['interval'])
+
+    haproxyview = HaproxyView(poller)
+    haproxyview.serve()
+
+if __name__ == '__main__':
+    main()
